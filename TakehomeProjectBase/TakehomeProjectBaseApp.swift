b5ec66c15fcebd5a25e69d875947fed1f4e3671d
twof <fabiobean2@gmail.com>
2024-03-23 23:58:11 -0700

    Initial Commit

---
+++ TakehomeProjectBase/TakehomeProjectBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TakehomeProjectBaseApp.swift
+//  TakehomeProjectBase
+//
+//  Created by fnord on 3/23/24.
+//
+
+import SwiftUI
+
+@main
+struct TakehomeProjectBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
