786272629b9ae5e409235070fab8fda4b496d0f9
twof <fabiobean2@gmail.com>
2024-03-28 23:44:01 -0700

    add macos platform

--- EverythingButTheBagelCore/Package.swift
+++ EverythingButTheBagelCore/Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
   name: "EverythingButTheBagelCore",
   platforms: [
-    .iOS(.v17)
+    .iOS(.v17), .macOS(.v14)
   ],
   products: [
     .library(
