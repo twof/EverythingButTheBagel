b954cbf24674b8506066f7fbdb7cb0e53a3f5db0
twof <fabiobean2@gmail.com>
2024-04-23 00:57:17 -0700

    Create new module for NASA's picture of the day api

---
+++ PictureOfTheDayCore/Package.swift
@@ -0,0 +1,47 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "PictureOfTheDayCore",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "PictureOfTheDayCore",
+            targets: ["PictureOfTheDayCore"]),
+    ],
+    dependencies: [
+      .package(
+        url: "https://github.com/pointfreeco/swift-composable-architecture",
+        branch: "shared-state-beta"
+      ),
+      .package(url: "https://github.com/getsentry/sentry-cocoa", from: "8.23.0"),
+      .package(url: "https://github.com/twof/FunctionSpy", branch: "main"),
+      .package(url: "https://github.com/twof/ControllableScrollView", from: "1.0.0"),
+      .package(url: "https://github.com/thebarndog/swift-dotenv.git", from: "2.0.0")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "PictureOfTheDayCore",
+            dependencies: [
+              .product(
+                name: "ComposableArchitecture",
+                package: "swift-composable-architecture"
+              ),
+              .product(name: "Sentry", package: "sentry-cocoa"),
+              .product(name: "ControllableScrollView", package: "controllablescrollview"),
+              .product(name: "SwiftDotenv", package: "swift-dotenv")
+            ]
+        ),
+        .testTarget(
+            name: "PictureOfTheDayCoreTests",
+            dependencies: [
+              "FunctionSpy",
+              "PictureOfTheDayCore"
+            ]
+        ),
+    ]
+)
