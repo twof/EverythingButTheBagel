44ff13397bac9b333868140801983417d77c5c47
twof <fabiobean2@gmail.com>
2024-06-05 21:57:07 -0700

    explaining localization setup

--- EverythingButTheBagelCore/Sources/EverythingButTheBagelCore/Localization/StringCatalogModel.swift
+++ EverythingButTheBagelCore/Sources/EverythingButTheBagelCore/Localization/StringCatalogModel.swift
@@ -1,13 +1,17 @@
 import Foundation
 
+/// Represents the contents of an .xcstrings file
 struct StringCatalogModel: Codable {
+  /// Locale code
   let sourceLanguage: String
+  /// String to list of localizations of that string
   let strings: [String: StringCatalogString]
   let version: String
 }
 
 struct StringCatalogString: Codable {
   let comment: String?
+  /// Locale codes to localized strings
   let localizations: [String: StringCatalogLocalization]?
 }
 
@@ -16,7 +20,9 @@ struct StringCatalogLocalization: Codable {
 }
 
 struct StringUnit: Codable {
+  /// State of the localized string ie translated
   let state: String
+  /// Localized string
   let value: String
 }
 
--- PictureOfTheDayCore/Sources/PictureOfTheDayCore/StringCatalog.swift
+++ PictureOfTheDayCore/Sources/PictureOfTheDayCore/StringCatalog.swift
@@ -3,6 +3,11 @@ import Foundation
 public extension URL {
   static var pictureOfTheDayStringCatalog: URL {
     let module = Bundle.module
+    // This is being loaded as JSON because Xcode refuses to copy .xcstrings files into the bundle
+    // SPM will actually do this, so it appears to be an Xcode specific bug. To work around that
+    // a build plugin is included in this package which copies the contents of
+    // Localizable.xcstrings to a new file called Localizable.json, which Xcode is happy to
+    // copy into the bundle.
     return module.url(forResource: "Localizable", withExtension: "json")!
   }
 }
